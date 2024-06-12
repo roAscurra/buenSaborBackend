20f6b72d8fab220c21c70a392827a0430d7aa148
roAscurra <rominaascurra4887@gmail.com>
2024-06-12 11:55:02 -0300

    Sucursales por idEmpresa

--- src/main/java/com/entidades/buenSabor/presentation/rest/SucursalController.java
+++ src/main/java/com/entidades/buenSabor/presentation/rest/SucursalController.java
@@ -3,7 +3,10 @@ package com.entidades.buenSabor.presentation.rest;
 
 import com.entidades.buenSabor.business.facade.Imp.SucursalFacadeImp;
 
+import com.entidades.buenSabor.business.facade.PedidoFacade;
+import com.entidades.buenSabor.business.facade.Sucursalfacade;
 import com.entidades.buenSabor.business.service.Base.BaseServiceImp;
+import com.entidades.buenSabor.domain.dto.pedido.PedidoFullDto;
 import com.entidades.buenSabor.domain.dto.provincia.ProvinciaFullDto;
 import com.entidades.buenSabor.domain.dto.sucursal.SucursalFullDto;
 
@@ -11,6 +14,7 @@ import com.entidades.buenSabor.domain.entities.Sucursal;
 import com.entidades.buenSabor.presentation.rest.Base.BaseControllerImp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +30,17 @@ public class SucursalController extends BaseControllerImp<Sucursal, SucursalFull
     public SucursalController(SucursalFacadeImp facade) {
         super(facade);
     }
-
+    @Autowired
+    private Sucursalfacade sucursalfacade;
+    @GetMapping("/empresa/{idEmpresa}")
+    public ResponseEntity<List<SucursalFullDto>> sucursalEmpresa(@PathVariable Long idEmpresa) {
+        List<SucursalFullDto> sucursales = sucursalfacade.sucursalEmpresa(idEmpresa);
+        if (sucursales != null && !sucursales.isEmpty()) {
+            return ResponseEntity.ok(sucursales);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
     @Override
     @PostMapping()
     @PreAuthorize("hasAuthority('ADMIN')")
--- src/main/java/com/entidades/buenSabor/repositories/SucursalRepository.java
+++ src/main/java/com/entidades/buenSabor/repositories/SucursalRepository.java
@@ -1,14 +1,18 @@
 package com.entidades.buenSabor.repositories;
 
+import com.entidades.buenSabor.domain.entities.Pedido;
 import com.entidades.buenSabor.domain.entities.Sucursal;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface SucursalRepository extends BaseRepository<Sucursal,Long> {
     @Query("SELECT s FROM Sucursal s LEFT JOIN FETCH s.promociones WHERE s.id = :id")
     Sucursal findWithPromocionesById(@Param("id") Long id);
-
+    @Query("SELECT p FROM Sucursal p WHERE p.empresa.id = :idEmpresa")
+    List<Sucursal> sucursalEmpresa(@Param("idEmpresa") Long idEmpresa);
 }
